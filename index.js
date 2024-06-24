f80eb0fe322ae93b88b560a8065c1804f46b81ba
Nanda Sholatul Akbar <nandasholatula@gmail.com>
2024-06-24 14:35:15 +0700

    update 1

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 const quranRouter = require('./routes/quran');
+const SurahRouter = require('./routes/surah');
+
 
 app.use(express.json());
 
 // Mount the Quran router at /quran
 app.use('/api/quran', quranRouter);
+
+
+app.use('/api/surah', SurahRouter);
 
 
 app.listen(PORT, () => {
