ec2e9dc7d17d3e58e1c4633f4cf96d772b0c28da
Nanda Sholatul Akbar <nandasholatula@gmail.com>
2024-06-24 15:05:56 +0700

    update 1

--- routes/surah.js
+++ routes/surah.js
@@ -1,23 +1,24 @@
-// routes/quran.js
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 
-const surahData = path.join(__dirname, '../data/quran.json');
+const surahDataPath = path.join(__dirname, '../data/quran.json');
+const surahData = JSON.parse(fs.readFileSync(surahDataPath, 'utf8'));
 
-// Endpoint to retrieve specific fields from data
+// Endpoint to retrieve specific ayah from data
 router.get('/:number/:ayahNumber', (req, res) => {
     const surahNumber = req.params.number;
     const ayahNumber = req.params.ayahNumber;
 
-    // Example: Fetch ayah data from your data source
+    // Find the surah in the surahData array
     const surah = surahData.find(surah => surah.number === parseInt(surahNumber));
 
     if (!surah) {
         return res.status(404).json({ error: 'Surah not found' });
     }
 
+    // Find the ayah within the found surah
     const ayah = surah.ayahs.find(ayah => ayah.number.inSurah === parseInt(ayahNumber));
 
     if (!ayah) {
@@ -26,6 +27,5 @@ router.get('/:number/:ayahNumber', (req, res) => {
 
     res.json(ayah); // Respond with the ayah data
 });
-
 
 module.exports = router;
